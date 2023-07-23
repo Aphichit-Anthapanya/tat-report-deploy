c2dc445ecc037601c9e82a4f2e74c70def96902e
1330x <aphichit_atpy@kkumail.com>
2023-07-23 22:16:59 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,13 +9,15 @@ export default function Home() {
     <Container as="main" className="d-flex content-body-wrapper d-flex flex-column">
       {/* and also work with module.scss (page.module.scss) */}
       <h1 className={styles["custom-text-green"]}>Home Page</h1>
+      <>ระบบแผนผล</>
       <Link href="/plan-report">plan-report</Link>
       <Link href="/plan-report/cpwm">รายงานสรุปงบประมาณรายโครงการเปรียบเทียบรายเดือน</Link>
       <Link href="/plan-report/group">รายงานสรุปงบประมาณตามแผนราย ด้าน ฝ่าย กอง สำนักงาน/งาน</Link>
       <Link href="/plan-report/summary">รายงานสรุปแผนปฏิบัติการ ททท รวมเล่ม</Link>
       <Link href="/plan-report/mistake">รายงานสรุปสถิติของหัวข้อที่เกิดความผิดพลาดบ่อยครั้ง</Link>
       <Link href="/plan-report/output">รายงานสรุปผลการดำเนินงานตามตัวชี้วัดกิจกรรม Output</Link>
-      <Link href="/purcase-system">ระบบจัดซื้อจัดจ้าง</Link>
+      <>ระบบจัดซื้อจัดจ้าง</>
+      <Link href="/purcase-system">หน้า search โครงการจัดซื้อจัดจ้าง</Link>
       <Link href="/purcase-system/add-action">เพิ่มงบ</Link>
       <Link href="/purcase-system/add-action">เพิ่มงบ(Calendar)</Link>
       <Link href="/purcase-system/view-project">view-โครงการจัดซื้อจัดจ้าง</Link>
